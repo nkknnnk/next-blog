df08af07a5b5ed4c6f6509a1ed4f126bf19fe2f6
nkknnnk <nk6593289@gmail.com>
2023-04-05 15:41:56 +0530

    initial commit

--- pages/contactus.tsx
+++ pages/contactus.tsx
@@ -31,7 +31,7 @@ const ContactUs = () => {
             <section className="text-gray-600 body-font relative">
                 <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
                     <div className="lg:w-2/3 md:w-1/2 w-full bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                        <iframe width="100%" height="100%" className="absolute inset-0" frameBorder="0" title="map" marginHeight="0" marginWidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{ filter: "grayscale(1) contrast(1.2) opacity(0.8)" }}></iframe>
+                        <iframe width="100%" height="100%" className="absolute inset-0" frameBorder={0} title="map" marginHeight={0} marginWidth={0} scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{ filter: "grayscale(1) contrast(1.2) opacity(0.8)" }}></iframe>
                         {hideAddress&&<div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
                             <span className='absolute right-2 top-0 p-3 cursor-pointer hover:hover:bg-sky-100' onClick={()=>setHideAddress(!hideAddress)}>X</span>
                             <div className="lg:w-1/2 px-6">
