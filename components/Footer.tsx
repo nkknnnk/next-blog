bd47cd5db7aa4142cfe856019486d0a880265535
nkknnnk <nk6593289@gmail.com>
2023-04-05 18:51:47 +0530

    initial commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     <span className="ml-3 text-xl">Next Blogs</span>
                 </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© {year} Nitish Kumar —
-                    <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@nitish</a>
+                    <a href='https://instagram.com/nitish_n09?igshid=ZDdkNTZiNTM=' className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@nitish</a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-500" target='_blank' href='https://www.facebook.com/profile.php?id=100078828485689'>
